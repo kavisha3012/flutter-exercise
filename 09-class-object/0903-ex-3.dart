b22a5197bdd9448cbefe142e462de91dbb292b4e
kavi2323 <kavishasutariya98@gmail.com>
2023-08-25 11:35:19 +0530

    today's lecture work exercise  add

--- 09-class-object/0903-ex-3.dart
+++ 09-class-object/0903-ex-3.dart
@@ -1,25 +1,70 @@
-class TextPrinter {
-  void bankName(String bname) {
-    print(bname);
+class BankDetail {
+  late String bankName;
+  late String branchName;
+  late int accNumber;
+  late String cityName;
+
+  void setBankName(
+      {required String name,
+      required String branch,
+      required int number,
+      required String city}) {
+    bankName = name;
+    branchName = branch;
+    accNumber = number;
+    cityName = city;
   }
 
-  void bankAccNo(int baccno) {
-    print(baccno);
+  void printDetails() {
+    print(bankName);
+    print(branchName);
+    print(accNumber);
+    print(cityName);
   }
 
-  void bankBranchName(String branchname) {
-    print(branchname);
-  }
-
-  void bankCity(String bcity) {
-    print(bcity);
+  BankDetail() {
+    bankName = '';
+    branchName = '';
+    accNumber = 0;
+    cityName = '';
   }
 }
 
 void main() {
-  TextPrinter printer = TextPrinter();
-  printer.bankName("HDFC");
-  printer.bankAccNo(364011223387);
-  printer.bankBranchName("Avenue Road");
-  printer.bankCity("Bangalore");
+  BankDetail bank = BankDetail();
+
+  bank.setBankName(
+      name: 'HDFC', branch: 'Don Chowck', number: 0102, city: 'Bhavnagar');
+
+  bank.printDetails();
 }
+
+
+
+// Second type :
+
+// class TextPrinter {
+//   void bankName(String bname) {
+//     print(bname);
+//   }
+
+//   void bankAccNo(int baccno) {
+//     print(baccno);
+//   }
+
+//   void bankBranchName(String branchname) {
+//     print(branchname);
+//   }
+
+//   void bankCity(String bcity) {
+//     print(bcity);
+//   }
+// }
+
+// void main() {
+//   TextPrinter printer = TextPrinter();
+//   printer.bankName("HDFC");
+//   printer.bankAccNo(364011223387);
+//   printer.bankBranchName("Avenue Road");
+//   printer.bankCity("Bangalore");
+// }
