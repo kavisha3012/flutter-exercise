b15c769435a2ac70860314c7054225d3b44c19c1
rudra99999 <rihstudwork@gmail.com>
2023-09-08 12:03:20 +0530

    add async,await,future ex

---
+++ 19-async-await-future/1901-ex-1.dart
@@ -0,0 +1,9 @@
+void main() async {
+  print("Line 1");
+  await display();
+  print("Line 3");
+}
+
+Future<void> display() async {
+  Future.delayed(Duration(seconds: 5), () => print("Line 2"));
+}
